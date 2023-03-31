55f5065174cb02c7d81f29baacab37ff3517d307
diego-zanella <108036471+diego-zanella@users.noreply.github.com>
2023-03-30 22:08:41 -0300

    Create app.py

---
+++ exemplo0/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+    return """  Olá
+                <br> 
+                professor"""
+
+if __name__ == "__main__":
+    app.run()
