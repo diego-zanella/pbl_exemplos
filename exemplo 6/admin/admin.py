3e37cdfec67781396aaec41b28a9071710cbc33b
diego-zanella <108036471+diego-zanella@users.noreply.github.com>
2023-03-30 22:32:17 -0300

    Create admin.py

---
+++ exemplo 6/admin/admin.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+admin = Blueprint("admin", __name__, static_folder="static", template_folder="templates")
+
+@admin.route("/")
+def admin_index():
+    return "Esse é seu modulo de admin"
